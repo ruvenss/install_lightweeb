42cba1e7c855063aecc88d4045aeaa4921225313
Ruvenss G. Wilches <ruvenss@gmail.com>
2024-03-17 21:51:17 +0100

    update

--- deploy_lightweb.sh
+++ deploy_lightweb.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 clear
-echo "Installing LightWeb..."
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+   sudo apt install -y shc > /dev/null
+   sudo apt install -y unzip > /dev/null
+   sudo apt install -y zip > /dev/null
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='macosx'
+   brew install shc
+fi
+echo "Installing LightWeb in $platform ..."
+echo "____________________________________"
+echo " Downloading LightWeb ...      | ⬇️ |"
+curl "https://github.com/ruvenss/lightweb/archive/refs/heads/master.zip" -o lightweb.zip
+echo " Unzipping LightWeb ...        | 🗜️ |"
+unzip lightweb.zip
+echo " Compiling LightWeb ...        | ⬆️ |"
+shc -f lightweb-master/lightweb.sh
+echo " Installing LightWeb ...       | ⬆️ |"
+sudo mv lightweb-master/lightweb /usr/local/bin
